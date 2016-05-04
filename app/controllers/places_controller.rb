95cf1d9ed6052f6587cb75f3a7251d3816dfcee1
Se7enb2st <ethanhe1992@gmail.com>
2016-05-04 17:56:09 +0000

    Added Validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -31,7 +35,11 @@ class PlacesController < ApplicationController
       return render text: "You Shall Not Pass!", status: :forbidden
     end
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -49,5 +57,4 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :address, :description)
   end
-
 end

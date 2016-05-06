241fcf9838de08ff6cdd843ccb96be5e594aa18e
Se7enb2st <ethanhe1992@gmail.com>
2016-05-06 17:51:58 +0000

    Set up comment database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name,:length => { minimum: 3 }, :presence => true
   validates :address,:length => { minimum: 10 }, :presence => true
   validates :description,:length => { minimum: 3 }, :presence => true
